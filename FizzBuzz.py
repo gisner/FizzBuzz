ab615c2ff39da1449178cc5db884f605d28b20d3
Thaslim <thaslim.kec@gmail.com>
2016-09-05 06:24:19 +0000

    Initial commit

---
+++ FizzBuzz.py
@@ -0,0 +1,21 @@
+#Fizz Buzz for n numbers
+import sys
+n=10
+try:
+    print("n value given: {}".format(sys.argv[1]))
+    n=int(sys.argv[1])
+except (IndexError,ValueError):  
+    try:
+        n=int(input("n is improper/non-numeric,Enter a numeric value for n: "))
+    except ValueError:
+        print("Again n is improper/non-numeric. Default value is taken: ",n)
+for i in range(1,n+1):
+    if i%15==0:
+        print("fizzbuzz")
+    elif i%3==0:
+        print("fizz")
+    elif i%5==0:
+        print("buzz")
+    else:
+        print(i)
+        
